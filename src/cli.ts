74e66aa8fc66354490803695cde3ed17f30bc6ab
ntriche <nate-riche@proton.me>
2024-07-08 12:03:54 -0400

    add a comment

--- src/cli.ts
+++ src/cli.ts
@@ -6,6 +6,9 @@ program
     .name("tumblr.ts cli")
     .description("A command line interface for the tumblr.ts library")
 
+// you can call this like so
+// node -r ts-node/register src/cli.ts test this-is-my-argument --boption this-is-my-option
+
 program.command('test')
     .description('A test command')
     .argument('<string>', 'stringy string')
